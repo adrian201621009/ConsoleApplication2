c51f188351fbcf026f2abdf97de9ebf58e5d5a12
adrian hernandez <fortaleza2012@hotmail.com>
2017-11-07 22:54:30 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApplication2/Calcular.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    public class Calcular
+    {
+        public int a { get; set; }
+        public int b { get; set; }
+        public double c { get; set; }
+        public double pi = 3.1416;
+
+        public void Area()
+        {
+            Console.WriteLine("introduce la altura del rectangulo:");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduce la base del rectangulo:");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("la area del rectangulo es:" + a * b);
+        }
+
+        public void Circulo()
+        {
+
+            Console.WriteLine("introduce el area del circulo:");
+            c = int.Parse(Console.ReadLine());
+            Console.WriteLine("el area  del circulo es:" + pi * (c * c));
+
+        }
+    }
+
+}
+
+
---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+       
+        
+            static void Main(string[] args)
+            {
+
+
+
+                Console.WriteLine("que desea realizar: 1) area del trinagulo 2) area  de un circulo");
+                int p = int.Parse(Console.ReadLine());
+
+                Calcular r = new Calcular() { };
+                if (p == 1) { r.Area(); }
+
+                if (p == 2) { r.Circulo(); }
+
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+
+
